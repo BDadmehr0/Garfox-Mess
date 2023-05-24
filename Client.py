0ee870cfac641f300ac05920433d2fbb7b0594ed
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-24 19:28:49 +0330

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,12 +1,46 @@
 import socket
+import threading
 
-target_host = "127.0.0.1"
-target_port = 80
+# آدرس و پورت سرور
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 1234
 
-client = socket.socket(socket.AF_INET, socket.SOCK.DGRAM)
+# لیست کلاینت‌ها
+clients = []
 
-client.sendto("AAABBBCCC", (target_host,target_port))
+def handle_client(client_socket, client_address):
+    while True:
+        try:
+            # دریافت پیام از کلاینت
+            message = client_socket.recv(1024).decode()
+            if message:
+                print(f'{client_address}: {message}')
+                # ارسال پیام به کلاینت‌های دیگر
+                for client in clients:
+                    if client != client_socket:
+                        client.send(message.encode())
+            else:
+                # اگر کلاینت قطع شود، آن را از لیست کلاینت‌ها حذف کنید
+                clients.remove(client_socket)
+                client_socket.close()
+                break
+        except:
+            # اگر خطایی رخ دهد، کلاینت را از لیست کلاینت‌ها حذف کنید
+            clients.remove(client_socket)
+            client_socket.close()
+            break
 
-data, addr = client.recvfrom(4096)
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((SERVER_ADDRESS, SERVER_PORT))
+    server_socket.listen(5)
+    print(f"سرور در حال گوش دادن بر روی {SERVER_ADDRESS}:{SERVER_PORT}")
 
-print data
+    while True:
+        client_socket, client_address = server_socket.accept()
+        clients.append(client_socket)
+        print(f"کلاینت جدید متصل شد: {client_address}")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
+
+start_server()
