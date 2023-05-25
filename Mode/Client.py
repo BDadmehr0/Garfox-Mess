391e537e0b75a9b602c2cb4b041fd80795440093
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-25 04:04:35 +0330

    Update

--- Mode/Client.py
+++ Mode/Client.py
@@ -1,8 +1,8 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'  # آدرس IP محلی
-PORT = 5500  # شماره پورت
+HOST = '127.0.0.1'
+PORT = 5500
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
@@ -11,7 +11,15 @@ def receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            print(message)
+            properties_start = message.find("[")
+            properties_end = message.find("]")
+            if properties_start != -1 and properties_end != -1:
+                properties_str = message[properties_start:properties_end+1]
+                properties = eval(properties_str)
+                message = message[:properties_start] + message[properties_end+1:]
+                print(f"{properties['name']} ({properties['address'][0]}:{properties['address'][1]}): {message}")
+            else:
+                print(message)
         except:
             print("Error occurred!")
             client.close()
@@ -20,10 +28,7 @@ def receive():
 def send():
     while True:
         message = input()
-        client.send(message.encode('utf-8'))
-
-receive_thread = threading.Thread(target=receive)
-send_thread = threading.Thread(target=send)
-
-receive_thread.start()
-send_thread.start()
+        if message.startswith('/private '):
+            recipient_name, message = message[9:].split(') ', 1)
+            for client, props in properties.items():
+                if props['name'] ==ادامه کد کلاینت:
--- Mode/Server.py
+++ Mode/Server.py
@@ -4,53 +4,46 @@ import threading
 HOST = '127.0.0.1'
 PORT = 5500
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
 
-clients = []
-properties = {}
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-def handle_client(client, address):
-    properties[client] = {"address": address, "name": ""}
+def receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            if message.startswith('/name '):
-                name = message[6:]
-                properties[client]["name"] = name
-                message = f"{name} joined the chat!"
-            elif message.startswith('/private '):
-                recipient_name, message = message[9:].split(') ', 1)
-                for client, props in properties.items():
-                    if props['name'] == recipient_name:
-                        recipient = client
-                        message = f"(private message) {message}"
-                        break
-                else:
-                    print("Recipient not found!")
-                    continue
-                recipient.send(f"{str(properties)}{message}".encode('utf-8'))
-            broadcast(f"{str(properties)}{message}".encode('utf-8'))
+            properties_start = message.find("[")
+            properties_end = message.find("]")
+            if properties_start != -1 and properties_end != -1:
+                properties_str = message[properties_start:properties_end+1]
+                properties = eval(properties_str)
+                message = message[:properties_start] + message[properties_end+1:]
+                print(f"{properties['name']} ({properties['address'][0]}:{properties['address'][1]}): {message}")
+            else:
+                print(message)
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            del properties[client]
+            print("Error occurred!")
             client.close()
             break
 
-def start():
-    server.listen()
-    print("Server isrunning...")
+def send():
     while True:
-        client, address = server.accept()
-        clients.append(client)
-        print(f"Connected with {str(address)}")
+        message = input()
+        if message.startswith('/private '):
+            recipient_name, message = message[9:].split(') ', 1)
+            for client, props in properties.items():
+                if props['name'] == recipient_name:
+                    recipient = client
+                    message = f"(private message) {message}"
+                    break
+            else:
+                print("Recipient not found!")
+                continue
+            recipient.send(f"{str(properties)}{message}".encode('utf-8'))
+        else:
+            client.send(f"{str(properties)}{message}".encode('utf-8'))
 
-        thread = threading.Thread(target=handle_client, args=(client, address))
-        thread.start()
+receive_thread = threading.Thread(target=receive)
+send_thread = threading.Thread(target=send)
 
-start()
+receive_thread.start()
+send_thread.start()
