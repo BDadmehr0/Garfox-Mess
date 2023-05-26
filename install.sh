4bb3e4c36b3339a33f5fee1bc41a044c99991300
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-27 03:14:04 +0330

    install.sh add futre and fix bugs

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 log () {
 
     now=$(date +"%Y-%m-%d %H:%M:%S")
@@ -21,41 +22,42 @@ EOF
 )
 
     echo "$from" > ./log/install.json
+    
 }
 
 log_loader () {
-    
+    installation () {
+	    echo "Installing python3-pip..."
+	    apt-get install -y python3-pip
+
+	    echo "Installing python3..."
+	    apt-get install -y python3
+
+	    echo "Installing required packages..."
+	    pip3 install -r requirements.txt
+
+	    echo "Copying Garfox-mess to /opt..."
+	    cp -r ./Garfox-mess /opt/Garfox-mess
+
+	    echo "Creating symlink..."
+	    ln -s /opt/Garfox-mess/GM.py /usr/local/bin/myapp
+	
+	    echo "Starting Garfox-mess..."
+	    python3 /opt/Garfox-mess/GM.py
+	
+	    echo "Installation completed."
+
+    }
     log_load=$(cat ./log/log.json)
 
     if [ -z "$log_load" ]
     then
         echo "Log file not found. Creating new log file."
+	installation
     else
         echo "Log file found."
     fi
 }
 
-installation () {
-    echo "Installing python3-pip..."
-    apt-get install -y python3-pip
-    echo "Installing python3..."
-    apt-get install -y python3
-
-    echo "Installing required packages..."
-    pip3 install -r requirements.txt
-
-    echo "Copying Garfox-mess to /opt..."
-    cp -r ./Garfox-mess /opt/Garfox-mess
-
-    echo "Creating symlink..."
-    ln -s /opt/Garfox-mess/GM.py /usr/local/bin/myapp
-
-    echo "Starting Garfox-mess..."
-    python3 /opt/Garfox-mess/GM.py
-
-    echo "Installation completed."
-}
-
 log_loader
 log
-installation
