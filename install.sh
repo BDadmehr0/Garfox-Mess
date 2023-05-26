82397967744f565c8abee2315cd9ca0d39be9fa5
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-26 07:38:57 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# نصب بسته‌های مورد نیاز
-apt-get install -y python3-pip
-apt-get install -y python3
+function log {
 
-# نصب وابستگی‌های پروژه
-pip3 install -r requirements.txt
+}
 
-# کپی فایل‌های پروژه به محل مورد نظر
-cp -r ./Garfox-mess /opt/Garfox-mess
+function log_loader {
 
-# تنظیمات و پیکربندی دیگر (به طور مثال، تنظیمات فایل‌های پیکربندی)
+}
 
-# ساخت لینک نمادین برای اجرای برنامه
-ln -s /opt/Garfox-mess/GM.py /usr/local/bin/myapp
+# apt-get install -y python3-pip
+# apt-get install -y python3
 
-python3 /opt/Garfox-mess/GM.py
 
-echo "Install Done"
+# pip3 install -r requirements.txt
+
+# cp -r ./Garfox-mess /opt/Garfox-mess
+
+# ln -s /opt/Garfox-mess/GM.py /usr/local/bin/myapp
+
+# python3 /opt/Garfox-mess/GM.py
+
+# echo "Install Done"
+
+greet log
