518bd4f531b8da50d3e9f331fea882805ab7ccda
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-28 00:24:57 +0330

    fix

--- Garfox_mess/Mode/Server.py
+++ Garfox_mess/Mode/Server.py
@@ -1,30 +1,34 @@
 def main_server():
-    import socket,select
+    import socket, select
 
     port = 12345
     socket_list = []
     users = {}
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('',port))
+    server_socket.bind(('', port))
     server_socket.listen(5)
     socket_list.append(server_socket)
+
     while True:
-        ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[],0)
+        ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [], 0)
         for sock in ready_to_read:
             if sock == server_socket:
                 connect, addr = server_socket.accept()
                 socket_list.append(connect)
-                connect.send("You are connected from:" + str(addr))
+                connect.send("You are connected from:".encode() + str(addr[0]).encode())
             else:
                 try:
-                    data = sock.recv(2048)
+                    data = sock.recv(2048).decode()
                     if data.startswith("#"):
-                        users[data[1:].lower()]=connect
-                        print("User " + data[1:] +" added.")
-                        connect.send("Your user detail saved as : "+str(data[1:]))
+                        users[data[1:].lower()] = sock
+                        print("User " + data[1:] + " added.")
+                        sock.send("Your user detail saved as : " + str(data[1:]).encode())
                     elif data.startswith("@"):
-                        users[data[1:data.index(':')].lower()].send(data[data.index(':')+1:])
+                        receiver = data[1:data.index(':')].lower()
+                        users[receiver].send(data[data.index(':')+1:].encode())
+                    else:
+                        sock.send("Invalid format".encode())
                 except:
                     continue
 
