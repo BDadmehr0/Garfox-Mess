73e259b8cf749ecb02ba918d4e9e773c688543d3
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-28 00:23:20 +0330

    fix byets

--- Garfox_mess/Mode/Client.py
+++ Garfox_mess/Mode/Client.py
@@ -10,19 +10,18 @@ def main_client():
     print(recv_msg)
 
     #send user details to server
-    send_msg = input("Enter your user name(prefix with #):")
-    client_socket.send(send_msg)
-
+    send_msg = input("Enter your user name(prefix with #)")
+    client_socket.send(send_msg.encode())
 
     #receive and send message from/to different user/s
-
     while True:
         recv_msg = client_socket.recv(1024)
-        print(recv_msg)
+        print(recv_msg.decode())  # تبدیل بایت به رشته و چاپ
+
         send_msg = input("Send your message in format [@user:message] ")
         if send_msg == 'exit':
             break
         else:
-            client_socket.send(send_msg)
+            client_socket.send(send_msg.encode())  # تبدیل رشته به بایت و ارسال به سرور
 
     client_socket.close()
