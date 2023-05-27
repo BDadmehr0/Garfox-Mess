1f5fbe98711644299a84e6f4fbef4098b0f7159a
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-27 03:31:36 +0330

    install.sh bugs

--- install.sh
+++ install.sh
@@ -1,63 +1,66 @@
 #!/bin/bash
 
+now=$(date +"%Y-%m-%d %H:%M:%S")
+hostname=$(hostname)
+os=$(uname)
 
-log () {
-
-    now=$(date +"%Y-%m-%d %H:%M:%S")
-    hostname=$(hostname)
-    os=$(uname)
-
-    from=$(cat <<EOF
+data=$(cat <<EOF
 {
-    "name":"Garfox-mess",
-    "version":"0.0.1",
-    "production-date":"2023-05-26",
-    "last-update":"$now",
-    "system" : {
-        "OS":"$os",
-        "host":"$hostname"
+    "name": "Garfox-mess",
+    "version": "0.0.1",
+    "production-date": "2023-05-26",
+    "last-update": "$now",
+    "system": {
+        "OS": "$os",
+        "host": "$hostname"
     }
 }
 EOF
 )
 
-    echo "$from" > ./log/install.json
-    
+echo "$data" > ./log/install.json
+
+installation () {
+    echo "Installing python3-pip..."
+    apt-get install -y python3-pip
+
+    echo "Installing python3..."
+    apt-get install -y python3
+
+    echo "Installing required packages..."
+    pip3 install -r requirements.txt
+
+    echo "Copying Garfox-mess to /opt..."
+    cp -r ./Garfox-mess /opt/Garfox-mess
+
+    echo "Creating symlink..."
+    ln -s /opt/Garfox-mess/GM.py /usr/local/bin/myapp
+
+    echo "Starting Garfox-mess..."
+    python3 /opt/Garfox-mess/GM.py
+
+    echo "Installation completed."
 }
 
-log_loader () {
-    installation () {
-	    echo "Installing python3-pip..."
-	    apt-get install -y python3-pip
-
-	    echo "Installing python3..."
-	    apt-get install -y python3
-
-	    echo "Installing required packages..."
-	    pip3 install -r requirements.txt
-
-	    echo "Copying Garfox-mess to /opt..."
-	    cp -r ./Garfox-mess /opt/Garfox-mess
-
-	    echo "Creating symlink..."
-	    ln -s /opt/Garfox-mess/GM.py /usr/local/bin/myapp
-	
-	    echo "Starting Garfox-mess..."
-	    python3 /opt/Garfox-mess/GM.py
-	
-	    echo "Installation completed."
-
-    }
+if [ -f ./log/log.json ]; then
     log_load=$(cat ./log/log.json)
-
-    if [ -z "$log_load" ]
-    then
+   if [[ -z "$log_load" ]]; then
         echo "Log file not found. Creating new log file."
-	installation
+        installation
     else
-        echo "Log file found."
+        read -p "Log file found. You have already installed. Do you want to install again? [y/N]" answer
+        if [[ "$answer" =~ ^[Yy]$ ]]; then
+            echo "$data" > ./log/install.json
+            installation
+        else
+            echo "Install Done"
+            exit
+        fi
     fi
-}
+else
+    echo "Log file not found. Creating new log file."
+    installation
+fi
 
-log_loader
-log
+echo "$data" > ./log/log.json
+
