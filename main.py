560376ee0809b67f47cd8b303c42e7ca13e0a431
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-07-27 19:08:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from mods.server import run_server
-
-print('\n(1) Server\n(2) Client')
+from mods.client import 
+print('\n(1) Server\n(2) Client\n\n(00) Exit')
 if __name__ == "__main__":
-    Q = int(input(''))
+    Q = int(input('-> '))
+
+    if Q == 1:
+        run_server()
+    elif Q == 2:
+
+    
